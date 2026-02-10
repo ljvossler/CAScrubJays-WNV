a2a73dfd7e02763aa435c8350edef375449f946f
ljvossler <ljvossler@arizona.edu>
2026-02-10 10:42:56 -0700

    adding full alignment pipeline slurm script

---
+++ submit_alignment_pipeline_array.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# --------------------
+### Directives Section
+# --------------------
+#SBATCH --job-name=submit_alignment_pipeline
+#SBATCH --account=mcnew
+#SBATCH --partition=standard
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=24:00:00
+#SBATCH --ntasks-per-node=12
+#SBATCH --output %x_%a.out
+#SBATCH --array=1-3
+
+INPUTFASTQ="$( sed "${SLURM_ARRAY_TASK_ID}q;d" INPUTFASTQS )"
+
+echo "\n"
+echo "|---------------Trimming ${INPUTFASTQ}---------------|"
+echo "\n"
+source Genomics-Main/A_Preprocessing/A0.1_trimming.sh -p params_preprocessing.sh -i ${INPUTFASTQ}
+
+echo "\n"
+echo "|---------------Aligning ${INPUTFASTQ}---------------|"
+echo "\n"
+source Genomics-Main/A_Preprocessing/A0.2_alignandsort.sh -p params_preprocessing.sh -i ${INPUTFASTQ}
+
+echo "\n"
+echo "|---------------Clipping Overlaps for ${INPUTFASTQ}---------------|"
+echo "\n"
+source Genomics-Main/A_Preprocessing/A0.3_ClipOverlap.sh -p params_preprocessing.sh -i ${INPUTFASTQ}
+
+echo "\n"
+echo "|---------------Realigning around Indels for ${INPUTFASTQ}---------------|"
+echo "\n"
+source Genomics-Main/A_Preprocessing/A0.4_indelrealignment.sh -p params_preprocessing.sh -i ${INPUTFASTQ}
+
+echo "\n"
+echo "|---------------Computing Bam Stats for ${INPUTFASTQ}---------------|"
+echo "\n"
+source Genomics-Main/A_Preprocessing/A0.5_bamstatistics.sh -p params_preprocessing.sh -i ${INPUTFASTQ}
+
+echo "\n"
+echo "|---------------Completed Alignment Pipeline for ${INPUTFASTQ}---------------|"
+echo "\n"
