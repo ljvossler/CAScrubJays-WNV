a316d5ac95b22a62d45bc11532c61b1a6e309683
ljvossler <ljvossler@arizona.edu>
2026-02-26 13:17:20 -0700

    minor base setup updates

--- params_base.sh
+++ params_base.sh
@@ -23,6 +23,7 @@ FILENAME_LIST="/path/to/list.txt" # list with sample codes associated with each 
 
 # define aspects of the reference genome
 CHRLEAD=NC_0 # characters at the start of a chromosome number (excluding scaffolds)
+SCAF_LEAD=NW_0 # characters at the start of a scaffold name
 MTCODE=NC_051467.1 # code for mitochondrial chromosome. changed this from sex chromosome since sex chromosomes are already excluded in base_setup SCAFFOLD list generation, but the MTs are included
 REF_ACC=GCF_041296385.1 # accession number for reference genome
 REF=/xdisk/mcnew/scrubjays_wnv/a_coerulescens_ncbi_ref_genome/data/GCF_041296385.1/GCF_041296385.1_UR_Acoe_1.0_genomic.fna # path to reference genome
