ad6661d3038d263039aeba0f27efbcb307d04916
ljvossler <ljvossler@arizona.edu>
2026-03-10 09:19:37 -0700

    quick and dirty edit to now organize SM run2 samples

--- organize_fastas.sh
+++ organize_fastas.sh
@@ -1,18 +1,29 @@
 #!/bin/sh
 
-# Concatenate SM Fasta files from all samples based on Read Number, standardize the names, and place merged fastq in fastqs_all directory
-FASTA_DIR=/xdisk/mcnew/scrubjays_wnv/aphelocoma_sequence_data/merged_fastqs_test
-sm_path='/xdisk/mcnew/scrubjays_wnv/aphelocoma_sequence_data/test_fastqs'
-for sample in $sm_path/*; 
+# Concatenate SM Fasta files from all samples across all runs based on Read Number, standardize the names, and place merged fastq in fastqs_all directory
+FASTA_DIR=/xdisk/mcnew/scrubjays_wnv/aphelocoma_sequence_data/fastqs_test
+sm_path_run1='/xdisk/mcnew/scrubjays_wnv/aphelocoma_sequence_data/SM-8328318_Run1_TS/fastqs'
+for sample in $sm_path_run1/*; 
 do 
 echo $sample
-cat $sample/*_R1_001.fastq.gz > $FASTA_DIR/$(basename $sample)_R1.fastq.gz
-cat $sample/*_R2_001.fastq.gz > $FASTA_DIR/$(basename $sample)_R2.fastq.gz; 
+cat $sample/*_R1_001.fastq.gz > $FASTA_DIR/$(basename $sample)_R1_run1.fastq.gz
+cat $sample/*_R2_001.fastq.gz > $FASTA_DIR/$(basename $sample)_R2_run1.fastq.gz; 
 done
 
+sm_path_run2='/xdisk/mcnew/scrubjays_wnv/aphelocoma_sequence_data/SM-8328318_Run2/fastqs'
+for sample in $sm_path_run2/*; 
+do 
+echo $sample
+cat $sample/*_R1_001.fastq.gz > $FASTA_DIR/$(basename $sample)_R1_run2.fastq.gz
+cat $sample/*_R2_001.fastq.gz > $FASTA_DIR/$(basename $sample)_R2_run2.fastq.gz; 
+done
+
+cat $FASTA_DIR/$(basename $sample)_R1_run1.fastq.gz $FASTA_DIR/$(basename $sample)_R1_run2.fastq.gz > $FASTA_DIR/$(basename $sample)_R1.fastq.gz && rm $FASTA_DIR/$(basename $sample)_R1_run1.fastq.gz $FASTA_DIR/$(basename $sample)_R1_run2.fastq.gz
+cat $FASTA_DIR/$(basename $sample)_R2_run1.fastq.gz $FASTA_DIR/$(basename $sample)_R2_run2.fastq.gz > $FASTA_DIR/$(basename $sample)_R2.fastq.gz && rm $FASTA_DIR/$(basename $sample)_R2_run1.fastq.gz $FASTA_DIR/$(basename $sample)_R2_run2.fastq.gz
 
 # Standardize the names of CCGP fasta files, and move to fastqs_all directory
 python3 <<'EOF'
+/
 import os
 ccgp_path='/xdisk/mcnew/scrubjays_wnv/aphelocoma_sequence_data/ccgp_sequences/public.hoffman2.idre.ucla.edu/aguillon/BHEMT'
 FASTA_DIR='/xdisk/mcnew/scrubjays_wnv/aphelocoma_sequence_data/fastqs_all'
@@ -25,4 +36,4 @@ EOF
 
 
 # create list of samples, assumes fastas are all formated with sample names as first term in an underscore separated string
-ls ${FASTA_DIR} | awk -F "_" '{print $1}' | sort -u > "/xdisk/mcnew/scrubjays_wnv/ljvossler/scrubjays_wnv/referencelists/test_sampleids.txt"
+ls ${FASTA_DIR} | awk -F "_" '{print $1}' | sort -u > "/xdisk/mcnew/scrubjays_wnv/ljvossler/scrubjays_wnv/referencelists/sampleids.txt"
