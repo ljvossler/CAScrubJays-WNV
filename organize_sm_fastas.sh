e0ef1b2419d5594739c47fed180cb191266c32bc
ljvossler <ljvossler@arizona.edu>
2026-04-11 09:06:38 -0700

    new test sm merge scriopt

---
+++ organize_sm_fastas.sh
@@ -0,0 +1,18 @@
+# Concatenate SM Fasta files from all samples
+
+sm_old_path1='/xdisk/mcnew/scrubjays_wnv/aphelocoma_sequence_data/sm_old/SM-8328318_Run1_TS/fastqs'
+sm_old_path2='/xdisk/mcnew/scrubjays_wnv/aphelocoma_sequence_data/sm_old/SM-8328318_Run2/fastqs'
+sm_new_path1='/xdisk/mcnew/scrubjays_wnv/aphelocoma_sequence_data/sm_new/SM_redo/Run1'
+sm_new_path2='/xdisk/mcnew/scrubjays_wnv/aphelocoma_sequence_data/sm_new/SM_redo/Run2'
+sm_merged_path='/xdisk/mcnew/scrubjays_wnv/aphelocoma_sequence_data/sm_merged'
+
+for sample in $(ls $sm_old_path/SM-8328318_Run1_TS/fastqs); 
+do 
+echo $sample
+
+echo COMMAND: cat $sm_old_path1/$sample/*_R1_001.fastq.gz $sm_old_path2/$sample/*_R1_001.fastq.gz $sm_new_path1/$sample/${sample}_R1.fastq.gz $sm_new_path1/$sample/${sample}_R1.fastq.gz > $sm_merged_path/${sample}_R1.fastq.gz
+cat $sm_old_path1/$sample/*_R1_001.fastq.gz $sm_old_path2/$sample/*_R1_001.fastq.gz $sm_new_path1/$sample/${sample}_R1.fastq.gz $sm_new_path1/$sample/${sample}_R1.fastq.gz > $sm_merged_path/${sample}_R1.fastq.gz
+
+echo COMMAND: cat $sm_old_path1/$sample/*_R2_001.fastq.gz $sm_old_path2/$sample/*_R2_001.fastq.gz $sm_new_path1/$sample/${sample}_R2.fastq.gz $sm_new_path1/$sample/${sample}_R2.fastq.gz > $sm_merged_path/${sample}_R2.fastq.gz
+cat $sm_old_path1/$sample/*_R2_001.fastq.gz $sm_old_path2/$sample/*_R2_001.fastq.gz $sm_new_path1/$sample/${sample}_R2.fastq.gz $sm_new_path1/$sample/${sample}_R2.fastq.gz > $sm_merged_path/${sample}_R2.fastq.gz
+done
