3e42890cab687da8faf1dfcd1c70af5cd89d8ae6
ljvossler <ljvossler@arizona.edu>
2026-04-24 14:34:36 -0700

    merge rmap fix

--- merge_rmaps.sh
+++ merge_rmaps.sh
@@ -9,6 +9,6 @@ for chr in $(cat ${OUTDIR}/referencelists/SCAFFOLDS.txt); do
     rmap_lst+=("${OUTDIR}/datafiles/linkage_map/${pop_prefix}_${chr}.rmap");
 done
 
-echo $rmap_lst
+echo "${rmap_lst[@]}"
 
-cat $rmap_lst > ${OUTDIR}/datafiles/linkage_map/${pop_prefix}_all.rmap
+cat $(echo "${rmap_lst[@]}") > ${OUTDIR}/datafiles/linkage_map/${pop_prefix}_all.rmap
