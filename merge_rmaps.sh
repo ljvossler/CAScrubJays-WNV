a8e63aeb381a7c701b7c3f3fbafc938b458ec0cb
ljvossler <ljvossler@arizona.edu>
2026-04-24 14:21:26 -0700

    merge rmap fix

--- merge_rmaps.sh
+++ merge_rmaps.sh
@@ -5,7 +5,10 @@ source params_base.sh
 pop_prefix=ccgp_jays_subset
 rmap_lst=()
 for chr in $(cat ${OUTDIR}/referencelists/SCAFFOLDS.txt); do
+    echo $chr
     rmap_lst+=(${OUTDIR}/datafiles/linkage_map/${pop_prefix}_${chr}.rmap);
 done
+
+echo $rmap_lst
 
 cat $rmap_lst > ${OUTDIR}/datafiles/linkage_map/${pop_prefix}_all.rmap
