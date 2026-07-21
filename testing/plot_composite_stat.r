aba1d6829bb6d4b1dced58e49f0fc7788b864f80
ljvossler <ljvossler@arizona.edu>
2026-07-21 11:39:45 -0700

    testing fst/tajima

---
+++ testing/plot_composite_stat.r
@@ -0,0 +1,66 @@
+# Load required packages, installing if necessary
+required_packages <- c("qqman", "hexbin", "readr", "ggrepel", "ggplot2", "dplyr", "RColorBrewer", "data.table")
+installed_packages <- rownames(installed.packages())
+
+cat("Checking required packages...\n")
+for (pkg in required_packages) {
+  if (!(pkg %in% installed_packages)) {
+    install.packages(pkg, repos = "http://forn.us.r-project.org")
+  }
+  library(pkg, character.only = TRUE)
+}
+
+outdir <- "/xdisk/mcnew/finches/dannyjackson/finches/analyses/composite_statistic/cra"
+color1 <- "#4EAFAF"
+color2 <- "#082B64"
+cutoff <- "0.001"  # Convert to numeric
+input <- "cra.composite_score.additive.with_chrnum.tsv"
+metric <- "composite_score"
+
+
+data <- fread(input, sep = "\t", na.strings = c("", "NA"), data.table = TRUE)
+
+metric_cutoff <- as.numeric(min(data$composite_score[data$highest_composite], na.rm = TRUE))
+
+data$position <- as.numeric(data$position)
+
+data[[metric]] <- as.numeric(data[[metric]])
+
+# Prepare data for plotting
+cat("Preparing data for plotting...\n")
+data$chromo <- factor(data$chromo, levels = c(1, "1A", 2:4, "4A", 5:29, "Z"))
+
+plot_data <- data %>%
+  group_by(chromo) %>%
+  summarise(chr_len = max(position)) %>%
+  mutate(tot = cumsum(chr_len) - chr_len) %>%
+  select(-chr_len) %>%
+  left_join(data, by = "chromo") %>%
+  arrange(chromo, position) %>%
+  mutate(BPcum = position + tot)
+
+axisdf <- plot_data %>%
+  group_by(chromo) %>%
+  summarize(center = mean(BPcum))
+
+
+# Plot
+cat("Generating plot...\n")
+ggplot(plot_data, aes(x = BPcum, y = !!sym(metric))) +
+  geom_point(aes(color = as.factor(chromo)), alpha = 0.8, size = 1) +
+  scale_color_manual(values = rep(c(color1, color2), length.out = length(unique(plot_data$chromo)))) +
+  scale_x_continuous(labels = axisdf$chromo, breaks = axisdf$center, guide = guide_axis(n.dodge = 2)) +
+  scale_y_continuous(expand = c(0, 0)) +
+  labs(x = "Chromosome", y = metric) +
+  # geom_hline(yintercept = metric_cutoff, linetype="dotted") +
+  theme_bw(base_size = 22) +
+  theme(
+    plot.title = element_text(hjust = 0.5),
+    legend.position = "none",
+    panel.border = element_blank(),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank()
+  )
+
+ggsave(filename = paste0(input, ".sigline.png"), 
+       width = 20, height = 5, units = "in")
