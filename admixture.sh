21de654e8baee2c7e13b1c21ac02018aecc8ec13
ljvossler <ljvossler@arizona.edu>
2026-07-30 09:06:15 -0700

    testing admixture script

---
+++ admixture.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 -p <parameter_file> -v <vcf_dir> -s <path_to_smc_file> -i <population_prefix> -n <num_haplotypes>
+
+This script generates pyrho lookup tables and outputs stats on optimal hyperparams to use for recombination mapping (A2.5.3) Requires csv outputs from SMC++
+
+Required argument:
+  -p  Path to the parameter file (e.g., params_preprocessing.sh in the GitHub repository).
+  -v  Path to chromosome-split vcfs"
+    exit 1
+fi
+
+# Parse command-line arguments
+while getopts ":p:v:o:k" option; do
+    case "${option}" in
+        p) PARAMS=${OPTARG};;
+        v) VCF=${OPTARG} ;;
+        o) OUTPREFIX=${OPTARG} ;;
+        k) NUM_K=${OPTARG} ;;
+        *) echo "Invalid option: -${OPTARG}" >&2; exit 1;;
+    esac
+done
+
+if [ -z "${PARAMS}" ]; then
+    echo "Error: No parameter file provided." >&2
+    exit 1
+fi
+
+# Load parameters
+source "${PARAMS}"
+
+printf "\n\n\n\n"
+date
+echo "Current script: A2.5.2_pyrho_recombination_params.sh"
+
+if [ -d "${OUTDIR}/analyses/admixture/" ];
+        then
+            echo "admixture directory already exists, moving on!"
+        else
+            echo "making admixture directory"
+            mkdir -p "${OUTDIR}/analyses/admixture/"
+fi
+
+# First convert VCF to PLINK .bed format
+plink --vcf ${VCF} --make-bed --out ${OUTDIR}/analyses/admixture/${OUTPREFIX}
+
+cd ${OUTDIR}/analyses/admixture/ # change dir since admixture apparently always outputs to working directory
+for k in $(seq ${NUM_K})
+do 
+    echo "Running admixture for K value: $k"
+    admixture --cv ${OUTDIR}/analyses/admixture/${OUTPREFIX}.bed ${k} | tee admixlog_${OUTPREFIX}_${k}.out
+    echo "Done running admixture for K $k"
+done
+
+echo "Done"
