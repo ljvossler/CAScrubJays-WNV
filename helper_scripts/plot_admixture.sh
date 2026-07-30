0e14b4a476e5ac37fcab4225055e19bc5a988839
ljvossler <ljvossler@arizona.edu>
2026-07-30 13:25:45 -0700

    update

---
+++ helper_scripts/plot_admixture.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+OUTPREFIX=alljays
+NUM_K=10
+POPS=cascrubjays
+
+source ../params_preprocessing.sh
+
+cd ${OUTDIR}/analyses/admixture
+
+# Collect all CV errors (https://speciationgenomics.github.io/ADMIXTURE/)
+awk '/CV/ {print $3,$4}' *out | cut -c 4,7-20 > ${OUTPREFIX}.cv.error
+awk '{split($1,name,"."); print $1,name[2]}' ${OUTPREFIX}.nosex > ${OUTPREFIX}.list
+
+#wget -P ${PROGDIR} https://github.com/speciationgenomics/scripts/raw/master/plotADMIXTURE.r
+Rscript ${PROGDIR}/plotADMIXTURE.r -p ${OUTPREFIX} -i ${OUTPREFIX}.list -k ${NUM_K} -l ${POPS}
