081c906bd87be375a35eeb94a35f4c59b5239eaa
ljvossler <ljvossler@arizona.edu>
2026-08-19 11:29:46 -0700

    td_diff script

--- helper_scripts/get_tajima_diff.sh
+++ helper_scripts/get_tajima_diff.sh
@@ -9,4 +9,4 @@ WIN=50000
 OUTPREFIX=alljays_tajimadiff
 
 (echo -e "chromo\tposition\tTajima"; awk 'BEGIN {OFS="\t"} NR==FNR && FNR>1 {data[$2,$3] = $9; next} FNR>1 && ($2,$3) in data {print $2, $3, $9 - data[$2,$3]}' ${TAJIMADIR}/${POP1}/${POP1}.Tajima.${WIN}.Ztransformed.csv ${TAJIMADIR}/${POP2}/${POP2}.Tajima.${WIN}.Ztransformed.csv) \
-| grep "${CHRLEAD}" | grep -v "${MTCODE}" >> ${TAJIMADIR}/${OUTPREFIX}.txt
+| grep -v "${SCAF_LEAD}" | grep -v "${MTCODE}" >> ${TAJIMADIR}/${OUTPREFIX}.txt
