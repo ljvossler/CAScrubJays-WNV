ba2610f9a862a0e5f531cd77435cf141538943a7
ljvossler <ljvossler@arizona.edu>
2026-08-20 19:03:41 -0700

    interpolate

--- helper_scripts/interpolate_cm_distances.py
+++ helper_scripts/interpolate_cm_distances.py
@@ -10,23 +10,15 @@ raw_plink_map = f'/xdisk/mcnew/scrubjays_wnv/ljvossler/scrubjays_wnv/datafiles/r
 final_plink_map = f'/xdisk/mcnew/scrubjays_wnv/ljvossler/scrubjays_wnv/datafiles/recombination_map/alljays_plink_{args.scaffold}.map'
 ref_linkage_map = '/xdisk/mcnew/scrubjays_wnv/ljvossler/scrubjays_wnv/referencelists/ref_linkage_map.txt'
 
-plink_map = pd.read_csv(raw_plink_map, header=None, names=['chrom_id', 'var_id', 'cm', 'bp'])
-ref_map = pd.read_csv(ref_linkage_map, sep='\t', header=None, names=['chrom_id', 'bp', 'cm'])
+plink_map = pd.read_csv(raw_plink_map, header=None, names=['chrom_id', 'var_id', 'cm', 'bp'], dtype={"chrom_id": str, "var_id": str, "cm": np.float64, "bp": np.int64})
+ref_map = pd.read_csv(ref_linkage_map, sep='\t', header=None, names=['chrom_id', 'bp', 'cm'], dtype={"chrom_id": str, "bp": np.int64, "cm": np.float64})
+ref_chrom_map = ref_map[ref_map["chrom_id"] == args.scaffold].sort_values(by="bp")
 
-# Interpolate cm values for each identified chromosome
-updated_rows = []
-for chrom, group in plink_map.groupby('chrom_id'):
-    print('current group: ' + group)
-    ref_sub = ref_map[ref_map['chrom_id'] == chrom]
-    
-    if ref_sub.empty:
-        group['cm'] = 0.0  # Default missing scaffolds to 0 (should be fine since we only care about known chroms downstream anyways)
-    else:
-        # interpolate cm values
-        group['cm'] = np.interp(group['bp'], ref_sub['bp'], ref_sub['cm'])
-        
-    updated_rows.append(group)
+# Interpolate cm values for one scaffold
+interpolated_cms = np.interp(plink_map['bp'].values, ref_chrom_map['bp'].values, ref_chrom_map['cm'].values, 
+                             left=ref_chrom_map['cm'].values[0], right=ref_chrom_map['cm'].values[-1])     
 
-map_df = pd.concat(updated_rows)
-map_df.to_csv(final_plink_map, sep='\t', index=False, header=False)
+plink_map['cm'] = interpolated_cms
+
+plink_map.to_csv(final_plink_map, sep='\t', index=False, header=False)
 print('done')
