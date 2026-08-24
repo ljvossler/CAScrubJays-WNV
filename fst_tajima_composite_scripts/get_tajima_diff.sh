c66d783079a35807349db4ff7376455f53a987cf
ljvossler <ljvossler@arizona.edu>
2026-08-24 12:42:17 -0700

    tajima_diff script

---
+++ fst_tajima_composite_scripts/get_tajima_diff.sh
@@ -0,0 +1,20 @@
+# Get Tajima's D Difference between populations
+
+source ../params_base.sh
+
+TAJIMADIR=${OUTDIR}/analyses/tajima
+POP1=alljays_pre
+POP2=alljays_post
+WIN=50000
+OUTPREFIX=alljays_tajimadiff
+
+echo "Calculating differences"
+(echo -e "chromo\tposition\tTajima"; awk 'BEGIN {OFS="\t"} NR==FNR && FNR>1 {data[$2,$3] = $9; next} FNR>1 && ($2,$3) in data {print $2, $3, $9 - data[$2,$3]}' ${TAJIMADIR}/${POP1}/${POP1}.Tajima.${WIN}.Ztransformed.csv ${TAJIMADIR}/${POP2}/${POP2}.Tajima.${WIN}.Ztransformed.csv) \
+| grep -v "${SCAF_LEAD}" | grep -v "${MTCODE}" >> ${TAJIMADIR}/${OUTPREFIX}.txt
+
+# Replace num_id with chrom_id
+echo "Replacing chromosome names based on conversion file..."
+while IFS=',' read -r first second; do
+    echo "Replacing $second with $first..."
+    sed "s/$second/$first/g" "${TAJIMADIR}/${OUTPREFIX}.txt" >> "${TAJIMADIR}/${OUTPREFIX}.chrom.txt" 
+done < "$CHR_FILE"
