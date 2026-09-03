27eef01b9c66fb4449dc95b6519d34243fb5e52c
ljvossler <ljvossler@arizona.edu>
2026-09-03 10:51:49 -0700

    commiting scripts for master gene list creation

--- helper_scripts/diamond.sh
+++ helper_scripts/diamond.sh
@@ -10,6 +10,12 @@ SUBJECT_FASTA=${REF}
 THREADS=12
 OUT_PATH=${OUTDIR}/datafiles/diamond
 
+sbatch --account=mcnew
+--job-name=rec_best_hits
+--partition=standard
+--output=slurm_output/%x.out
+--nodes=1
+--ntasks-per-node=${THREADS}
+--time=24:00:00
 Rscript ${SCRIPTDIR}/helper_scripts/reciprocal_best_hits.r ${QUERY_FASTA} ${SUBJECT_FASTA} ${THREADS} ${OUT_PATH}
-
 echo "DONE"
