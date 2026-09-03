1ab17b6bc9dffc5bc8a46580ab943ad7b11db777
ljvossler <ljvossler@arizona.edu>
2026-09-03 12:59:24 -0700

    commiting scripts for master gene list creation

--- helper_scripts/diamond.sh
+++ helper_scripts/diamond.sh
@@ -1,20 +1,24 @@
-#!/bin/sh
+#!/bin/bash
+# --------------------
+### Directives Section
+# --------------------
+#SBATCH --job-name=rec_best_hits
+#SBATCH --account=mcnew
+#SBATCH --partition=standard
+#SBATCH --nodes=1
+#SBATCH --ntasks=8
+#SBATCH --time=24:00:00
+#SBATCH --output ../slurm_outs/%x.out
 
-# Load parameters
+
 source "/xdisk/mcnew/scrubjays_wnv/programs/CAScrubJays-WNV/params_base.sh"
+module load R/4.5.2
 
 QUERY_FASTA=/xdisk/mcnew/scrubjays_wnv/human_GRCh38_dataset/ncbi_dataset/data/GCF_000001405.40/GCF_000001405.40_GRCh38.p14_genomic.fna
 SUBJECT_FASTA=${REF}
 THREADS=8
 OUT_PATH=${OUTDIR}/datafiles/diamond
 
-sbatch --account=mcnew
---job-name=rec_best_hits
---partition=standard
---output=../slurm_output/%x.out
---nodes=1
---ntasks-per-node=${THREADS}
---time=24:00:00
-module load R/4.5.2
+
 Rscript ${SCRIPTDIR}/helper_scripts/reciprocal_best_hits.r ${QUERY_FASTA} ${SUBJECT_FASTA} ${THREADS} ${OUT_PATH}
 echo "DONE"
