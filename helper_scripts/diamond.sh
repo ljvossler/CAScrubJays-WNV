3bea5f511f6659eb1ddf2bb306e7ea5fe253761d
ljvossler <ljvossler@arizona.edu>
2026-09-04 11:31:21 -0700

    diamond script

--- helper_scripts/diamond.sh
+++ helper_scripts/diamond.sh
@@ -36,6 +36,6 @@ cd ${OUT_PATH}
 #diamond blastp -q ${SCRUBJAY_FAA} -d human.dmnd -o reverse_search.tsv --ultra-sensitive
 
 # Get Best Hits
-${PROGDIR}/reciprologs/reciprologs ${HUMAN_FAA} ${SCRUBJAY_FAA} diamondp --ultra-sensitive
+${PROGDIR}/reciprologs/reciprologs ${HUMAN_FAA} ${SCRUBJAY_FAA} diamondp
 
 echo "DONE"
